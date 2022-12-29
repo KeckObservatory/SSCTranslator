317c37b952eb523e3a551daf183ae6ab75ece5a3
lchjoel1031 <lchjoel1031@gmail.com>
2022-12-28 20:51:06 -1000

    Update execute_observation.py

--- ddoi_script_functions/execute_observation.py
+++ ddoi_script_functions/execute_observation.py
@@ -27,62 +27,10 @@ class ExecuteObservation(KPFTranslatorFunction):
         logger.debug(f'Setting exposure time to {new_exptime:.1f} ms')
         ITIMEkw.write(new_exptime)
 
-        # Set coadds
-        COADDSkw = ktl.cache(service='mds', keyword='COADDS')
-        logger.debug(f'Setting coadds to {int(args.coadds)}')
-        COADDSkw.write(int(input))
-    
-        # Set sampling
-        
-        namematch = re.match('(M?CDS)(\d*)', input.strip())
-        if namematch is None:
-            raise DDOIMissingArgumentException(f'Unable to parse "{args.sampmode}"')
-        mode = {'CDS': 2, 'MCDS': 3}.get(namematch.group(1))
-
-        SAMPMODEkw = ktl.cache(service='mds', keyword='SAMPMODE')
-        NUMREADSkw = ktl.cache(service='mds', keyword='NUMREADS')
-        SAMPMODEkw.write(mode)
-        if mode == 3:
-            nreads = int(namematch.group(2))
-            NUMREADSkw.write(nreads)
-        
-        # Set Object
-        objectkw = ktl.cache(service='mds', keyword='OBJECT')
-        objectkw.write(args.object)
-
-        # Update FCS
-
-        ROTPPOSNkw = ktl.cache(keyword='ROTPPOSN', service='dcs')
-        ROTPPOSN = float(ROTPPOSNkw.read())
-        ELkw = ktl.cache(keyword='EL', service='dcs')
-        EL = float(ELkw.read())
-
-        FCPA_ELkw = ktl.cache(keyword='PA_EL', service='mfcs')
-        FCPA_ELkw.write(f"{ROTPPOSN:.2f} {EL:.2f}")
-
-        FCPA_ELkw = ktl.cache(keyword='PA_EL', service='mfcs')
-        FCPA_EL = FCPA_ELkw.read()
-        FCSPA = float(FCPA_EL.split()[0])
-        FCSEL = float(FCPA_EL.split()[1])
-        
-        ROTPPOSNkw = ktl.cache(keyword='ROTPPOSN', service='dcs')
-        ROTPPOSN = float(ROTPPOSNkw.read())
-        ELkw = ktl.cache(keyword='EL', service='dcs')
-        EL = float(ELkw.read())
-        done = np.isclose(FCSPA, ROTPPOSN, atol=args.PAthreshold)\
-            and np.isclose(FCSEL, EL, atol=args.ELthreshold)
-        
-        if not done:
-            logger.warn("Unable to update FCS. Exiting")
-            return
-        
-        # Pad time to ensure proper execution
-        sleep(1)
-
         # Expose!
-        GOkw = ktl.cache(service='magiq', keyword='camcmd=start')
+        GOkw = ktl.cache(service='magiq', keyword='camcmd')
         logger.info('Starting exposure')
-        GOkw.write(True)
+        GOkw.write(start)
         
         return
 #        raise NotImplementedError()
