bbf553a6e3fddfba6d1d480a7e9893d754c97942
Max Brodheim <max.brodheim@gmail.com>
2023-03-15 14:54:47 -1000

    class fix

--- ssc/SSCTranslatorFunction.py
+++ ssc/SSCTranslatorFunction.py
@@ -18,7 +18,7 @@ class SSCTranslatorFunction(TranslatorModuleFunction):
 
         return config_files
     
-    @staticmethod
+    @classmethod
     def check_inputs(cls, args, key, allowed_types):
         if args[key]:
             if type(args[key]) in allowed_types:
