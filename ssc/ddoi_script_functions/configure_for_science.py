598e41b8e7ac79c6ef99be85a21c6d8c16db4fc1
Max Brodheim <max.brodheim@gmail.com>
2023-04-18 09:28:52 -1000

    implement configure_for_science

--- ssc/ddoi_script_functions/configure_for_science.py
+++ ssc/ddoi_script_functions/configure_for_science.py
@@ -1,8 +1,11 @@
 import ktl
-from kpf.KPFTranslatorFunction import KPFTranslatorFunction
+from ssc.SSCTranslatorFunction import SSCTranslatorFunction
 
+from ssc.imager.SetBinning import SetBinning
+from ssc.imager.SetExptime import SetExptime
+from ssc.imager.ToggleCamera import ToggleCamera
 
-class configure_for_science(KPFTranslatorFunction):
+class configure_for_science(SSCTranslatorFunction):
     '''
     '''
     @classmethod
@@ -11,7 +14,23 @@ class configure_for_science(KPFTranslatorFunction):
 
     @classmethod
     def perform(cls, args, logger, cfg):
-        raise NotImplementedError()
+
+        # Extract our needed arguments (exptime and binning)
+        sequence = args.get('sequence')
+        parameters = sequence.get('paramters')
+        exptime = parameters.get('det1_exp_time')
+        binning = parameters.get('det1_binning')
+
+        # Set the values
+        SetBinning.execute({'exptime' : binning})
+        SetExptime.execute({'binning' : exptime})
+        
+        # Tell MAGIQ to load those values
+        cls.set_magiq_cmd(logger)
+
+        # Start the camera
+        ToggleCamera.execute({'status' : 'start'})
+
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
