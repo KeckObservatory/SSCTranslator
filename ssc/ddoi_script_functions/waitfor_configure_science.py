b706da858d511eeb0fdbbf6624f80a7e43414a7f
Tyler Tucker <tylertucker202@gmail.com>
2023-04-19 13:17:22 -1000

    waitfor sleeps for a sec

--- ssc/ddoi_script_functions/waitfor_configure_science.py
+++ ssc/ddoi_script_functions/waitfor_configure_science.py
@@ -1,5 +1,6 @@
 import ktl
 from ssc.SSCTranslatorFunction import SSCTranslatorFunction
+import time
 
 
 class waitfor_configure_science(SSCTranslatorFunction):
@@ -11,7 +12,7 @@ class waitfor_configure_science(SSCTranslatorFunction):
 
     @classmethod
     def perform(cls, args, logger, cfg):
-        raise NotImplementedError()
+        time.sleep(1)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
